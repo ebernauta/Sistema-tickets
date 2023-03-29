cdac3375a514cdca4e17438cf6fefd2887f2fa83
Ebernauta <ebernauta18@gmail.com>
2023-03-29 09:18:52 -0300

    Se agrega para usuarios: modificar, eliminar y crear

--- app.py
+++ app.py
@@ -93,14 +93,14 @@ def loginPanel():
             print("debajo del redirect")
         else:
             flash("Contraseña maestra incorrecta...", 'danger')
-            return render_template('loginPanel.html')
+            return render_template('auth/loginPanel.html')
     else:
-        return render_template('loginPanel.html') 
+        return render_template('auth/loginPanel.html') 
 
 @app.route('/panelAdministracion', methods=['GET', 'POST'])
 @login_required
 def panelAdmin():
-    return render_template('panelAdmin.html')
+    return render_template('panel/panelAdmin.html')
     
    
 
@@ -109,20 +109,67 @@ def panelAdmin():
 def usuariosPanel():
     listaUsuarios = ModelUser.allUsers(db)
     print(listaUsuarios)
-    return render_template('usuariosPanel.html', filas=listaUsuarios)
+    return render_template('panel/usuariosPanel.html', filas=listaUsuarios)
 
-@app.route('/panelAdministracion/usuarios/agregarUsuario', methods=['GET', 'POST'])
+@app.route('/newUser', methods=['GET', 'POST'])
 @login_required
-def agregarUsuarios():
+def newUser():
     if request.method == 'POST':
-        cursor = db.connection.cursor()
+        flash("Se ha creado un nuevo usuario !", "success")
         username = request.form['username']
         password = request.form['password']
         fullname = request.form['fullname']
         nuevoUsuario = ModelUser.newUser(db, username, password, fullname)
-        return redirect(url_for('agregarUsuarios'))
+        return redirect(url_for('usuariosPanel'))
+
+
+@app.route('/delete/<int:id_data>', methods=['GET'])
+def deleteUsuario(id_data):
+    flash("Usuario eliminado exitosamente !", "success")
+    cursor = db.connection.cursor()
+    sql = """ DELETE FROM user WHERE id='{}'""".format(id_data)
+    cursor.execute(sql)
+    return redirect(url_for('usuariosPanel'))
+
+@app.route('/updateUsuario', methods=['POST', 'GET'])
+@login_required
+def updateUsuario():
+    if request.method == 'POST':
+        flash("Usuario editado exitosamente !","success")
+        id_data = request.form['id']
+        username = request.form['username']
+        password = request.form['password']
+        fullname = request.form['fullname']
+        updateSql = ModelUser.editUser(db, username, password, fullname, id_data)
+        return redirect(url_for('usuariosPanel'))
+        
+        
+
+
+@app.route('/panelAdministracion/departamentos')
+@login_required
+def departametosPanel():
+    cursor = db.connection.cursor()
+    sql = """SELECT * FROM departamentos"""
+    cursor.execute(sql)
+    listaDepartamentos = cursor.fetchall()
+    print(listaDepartamentos)
+    return render_template('panel/departamentosPanel.html', departamentos= listaDepartamentos)
+
+@app.route('/panelAdministracion/departamentos/agregarDepartamento', methods=['GET', 'POST'])
+@login_required
+def agregarDepartamentos():
+    if request.method == 'POST':
+        departamento = request.form['departamento']
+        cursor = db.connection.cursor()
+        sql = """INSERT INTO departamentos (iddepartamento, departamento) VALUES 
+                    (null, '{}')""".format(departamento)
+        cursor.execute(sql)
+        flash('Se ha agregado un nuevo departamento !', 'success')
+        return redirect(url_for('panel/agregarDepartamentos'))
     else:
-        return render_template('agregarUsuarios.html')
+        return render_template('panel/agregarDepartamentos.html')
+
 
 
 @app.route('/protected')
